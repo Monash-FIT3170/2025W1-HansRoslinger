44cf1cee331e5a08949de0576c0d28a7feeb37fb
Connor Macdougall <cmac0046@student.monash.edu>
2025-04-07 22:09:24 +1000

    1-4 completed, all working

---
+++ spikeApp/imports/ui/App.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+import { InputField, Button } from './Input/index.jsx';
+import { TextBox } from './Output/index.jsx'; // Importing TextBox component to display output
+
+export const App = () => {
+  const [item, setItem] = useState('Apple'); // useState is a very common react function that defines a variable 'item' and a function 'setItem' that is used to modify that item
+  const [quantity, setQuantity] = useState('5');
+
+  const handleItemChange = (e) => setItem(e.target.value); // Here we have defined a function which will update the value of the item variable when we enter anything into its InputField
+  const handleQuantityChange = (e) => setQuantity(e.target.value);
+
+  const [shoppingList, setShoppingList] = useState<string[]>([]); //we've define a list that can we set the value of
+  const addToList = () => { //this function updates the shopping list above by appending a new value to it
+  setShoppingList((prevList) => [...prevList, `Item: ${item}, Quantity: ${quantity}`]);
+  }
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <Hello />
+      <Info />
+
+      {/* Input for item */}
+      <InputField
+        label="Item"
+        type="text"
+        value={item} // The value of the item is displayed to the text box
+        onChange={handleItemChange} // When you type into the text box, the value is updated
+      />
+
+      {/* Input for quantity */}
+      <InputField
+        label="Quantity"
+        type="number"
+        value={quantity}
+        onChange={handleQuantityChange}
+      />
+
+      <br></br>
+      <br></br>
+
+      <Button
+      label="Add to List"
+      onClick={addToList}
+      />
+      <TextBox value={shoppingList.join("\n")} /> 
+    </div> // TextBox will display the current values
+  );
+};

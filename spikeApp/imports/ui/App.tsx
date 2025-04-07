47716c45b0f3bbdb5b1b13d51e9674c63d97578d
Connor Macdougall <cmac0046@student.monash.edu>
2025-04-07 22:21:21 +1000

    Finished initial spike

--- spikeApp/imports/ui/App.tsx
+++ spikeApp/imports/ui/App.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-import { InputField, Button } from './Input/index.jsx';
-import { TextBox } from './Output/index.jsx'; // Importing TextBox component to display output
+import { Hello } from './Hello';
+import { Info } from './Info';
+import { InputField, Button } from './Input';
+import { TextBox } from './Output'; // Importing TextBox component to display output
+import { ShoppingListCollection, AddToList } from '../api/shoppinglist';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 
-export const App = () => {
-  const [item, setItem] = useState('Apple'); // useState is a very common react function that defines a variable 'item' and a function 'setItem' that is used to modify that item
-  const [quantity, setQuantity] = useState('5');
+export const App: React.FC = () => {
+  const [item, setItem] = useState<string>('Apple'); //useState is a very common react function that defines a variable 'item' and a function 'setitem' that is used to modify that item
+  const [quantity, setQuantity] = useState<string>('5'); //Here we set the default value of the quantity to "5"
 
-  const handleItemChange = (e) => setItem(e.target.value); // Here we have defined a function which will update the value of the item variable when we enter anything into its InputField
-  const handleQuantityChange = (e) => setQuantity(e.target.value);
+  const handleItemChange = (e: React.ChangeEvent<HTMLInputElement>) => setItem(e.target.value); //here we have defined a function which will update the value of the item variable when we enter anything into it's InputField
+  const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => setQuantity(e.target.value);
 
-  const [shoppingList, setShoppingList] = useState<string[]>([]); //we've define a list that can we set the value of
-  const addToList = () => { //this function updates the shopping list above by appending a new value to it
-  setShoppingList((prevList) => [...prevList, `Item: ${item}, Quantity: ${quantity}`]);
-  }
+  const [shoppingList, setShoppingList] = useState<{ item: string; quantity: string }[]>([]);  // We've defined a list that can hold strings
+  
+  useTracker(() => {
+    Meteor.subscribe('shoppingList'); // Subscribe to the shopping list data
+    const items = ShoppingListCollection.find().fetch(); // Fetch the shopping list items
+    setShoppingList(items); // Update the shopping list state
+  }, []);
 
   return (
     <div>
@@ -25,15 +31,13 @@ export const App = () => {
       {/* Input for item */}
       <InputField
         label="Item"
-        type="text"
-        value={item} // The value of the item is displayed to the text box
-        onChange={handleItemChange} // When you type into the text box, the value is updated
+        value={item} //The value of the item is displayed to the text box
+        onChange={handleItemChange} //When you type into the text box, the value is updated
       />
 
       {/* Input for quantity */}
       <InputField
         label="Quantity"
-        type="number"
         value={quantity}
         onChange={handleQuantityChange}
       />
@@ -42,10 +46,14 @@ export const App = () => {
       <br></br>
 
       <Button
-      label="Add to List"
-      onClick={addToList}
+        label="Add to List"
+        onClick={() => AddToList(item, quantity)}
       />
-      <TextBox value={shoppingList.join("\n")} /> 
-    </div> // TextBox will display the current values
+      <br></br>
+      <br></br>
+
+      {/* Display the current items and quantity in the shopping list */}
+      <TextBox value={shoppingList.map(entry => `Item: ${entry.item}, Quantity: ${entry.quantity}`).join('\n')} />
+    </div>
   );
 };

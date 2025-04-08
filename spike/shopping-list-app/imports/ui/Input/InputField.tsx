8d28ffa07a8810ee4b584b60eee089c17cc4d044
Harkirat <89556782+Harkie21@users.noreply.github.com>
2025-04-08 13:47:41 +1000

    Finalised Technical Spike

---
+++ spike/shopping-list-app/imports/ui/Input/InputField.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const InputField: React.FC<{ label: string; value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }> = ({ label, value, onChange }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <br />
+      <input type='text' value={value} onChange={onChange} />
+    </div>
+  );
+};

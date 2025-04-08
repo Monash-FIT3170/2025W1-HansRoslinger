fe49b0d3d225db61990c4e94724afefc691ca096
Naaaaaathan <nathan.michailidis@gmail.com>
2025-04-08 22:08:43 +1000

    Technology Tutorial done

---
+++ spike/shopping-list-app/imports/ui/Output/TextBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react'; 
+export const TextBox: React.FC<{ value: string }> = ({ value }) => { return ( 
+    <div> 
+        <textarea value={value} readOnly /> 
+    </div> 
+); 
+}; 

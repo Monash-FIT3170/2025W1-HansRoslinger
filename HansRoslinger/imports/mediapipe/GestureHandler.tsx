9e2b69b496e5284769caaf3940cbe578285d6a0e
Liam <lchu0032@student.monash.edu>
2025-04-25 11:05:30 +1000

    Adding the skeleton of the class GestureHandler

--- HansRoslinger/imports/mediapipe/GestureHandler.tsx
+++ HansRoslinger/imports/mediapipe/GestureHandler.tsx
@@ -1,3 +1,35 @@
-const GestureHandler = () => {};
+import React, { createContext, useContext, useRef } from 'react';
+
+
+// Default parameters for gestures to be handled
+const DEFAULT_MIN_CERTAINTY = 0.6;
+const DEFAULT_DURATION = 500;
+
+
+const GestureHandler = () => {
+
+
+
+    // Maps the Gestures 
+    const registerGesture = () => {
+
+
+
+
+
+    };
+
+    // Checks and Validates the parameters of incoming gestures
+    const processGestures = () => {
+
+
+
+
+    };
+
+
+
+
+};
 
 export default GestureHandler;

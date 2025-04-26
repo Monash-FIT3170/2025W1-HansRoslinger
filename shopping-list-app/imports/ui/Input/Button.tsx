f0b513ea6d45b270bb1cf70a5695987d041aba47
sheharahewawasam <sheharh2005@gmail.com>
2025-04-26 13:59:22 +1000

    finished shopping list app

---
+++ shopping-list-app/imports/ui/Input/Button.tsx
@@ -0,0 +1,9 @@
+// Input/Button.tsx
+import React from 'react';
+
+// Button component that takes 'label' and 'onClick' as props
+export function Button({ label, onClick }) {
+  return (
+    <button onClick={onClick}>{label}</button>
+  );
+}
---
+++ shopping-list-app/imports/ui/Input/index.tsx
@@ -0,0 +1,2 @@
+export { InputField } from './InputField';
+export { Button} from './Button';

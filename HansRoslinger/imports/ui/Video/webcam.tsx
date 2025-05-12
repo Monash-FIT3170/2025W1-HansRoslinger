52cd89309220c47161351e57b8838282bb8146c8
Connor Macdougall <cmac0046@student.monash.edu>
2025-05-12 20:36:05 +1000

    Feat: Add gesture detector within each Webcam component

--- HansRoslinger/imports/ui/Video/webcam.tsx
+++ HansRoslinger/imports/ui/Video/webcam.tsx
@@ -1,15 +1,20 @@
-import React from 'react'; // Import React
+import React, { useRef } from 'react'; // Import React
 import Webcam from 'react-webcam';
 import './Webcam.css';
+import GestureDetector from '/imports/mediapipe/gestures';
 
 interface WebcamProps {
   grayscale: boolean;
 }
 
 export const WebcamComponent: React.FC<WebcamProps> = ({ grayscale }) => {
+  const webcamRef = useRef<Webcam | null>(null);
+
+  GestureDetector(webcamRef);
+
   return (
     <div className={`webcam-container ${grayscale ? 'grayscale' : ''}`}>
-      <Webcam className="webcam-view"/>
+      <Webcam ref={webcamRef} className="webcam-view"/>
     </div>
   );
 };

3cbb32ef210dab6eb8a8d02faebbef287dba2a13
mcra0009 <mcra0009@student.monash.edu>
2025-05-18 18:50:16 +1000

    fix eslint

---
+++ HansRoslinger/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    // your rules here
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};

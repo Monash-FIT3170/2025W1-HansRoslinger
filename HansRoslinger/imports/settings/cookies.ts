fea7838e63c6df9f9d2e7de321a1c171db7cfb85
Naaaaaathan <nathan.michailidis@gmail.com>
2025-05-21 08:09:11 +1000

    cookie api in progress

--- HansRoslinger/imports/settings/cookies.ts
+++ HansRoslinger/imports/settings/cookies.ts
@@ -1,12 +1,25 @@
-export const getCookie = (name: string): string | null => {
-  if (typeof document === 'undefined') return null;
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  return parts.length === 2 ? parts.pop()?.split(';').shift() || null : null;
-};
+import { Cookies } from 'meteor/ostrio:cookies';
+const timeout = 1000*60*60
 
-export const setCookie = (name: string, value: string, days = 30) => {
-  const date = new Date();
-  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-  document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/;SameSite=Lax`;
-};
+export async function encrypt(payload: any) {
+  return payload
+}
+  
+export async function decrypt(payload: any) {
+  return payload
+}
+
+export async function setCookie(name: string, data: any) {
+    const expires = new Date(Date.now() + timeout)
+    const info = await encrypt({ data, expires });
+    Cookies().set(name, info, { expires, httpOnly: true });
+}
+
+export async function getCookie(name: string) {
+    const info = Cookies().get(name)?.value;
+    if (!info) {
+        return null;
+    }
+    const data = await decrypt(info);
+    return data
+}

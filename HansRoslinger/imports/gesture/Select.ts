f3475c2849fb367e81de01b60d0eb844b2bd960c
mcra0009 <mcra0009@student.monash.edu>
2025-08-29 09:04:13 +1000

    FIXED

--- HansRoslinger/imports/gesture/Select.ts
+++ HansRoslinger/imports/gesture/Select.ts
@@ -1,6 +1,10 @@
 import { Gesture, gestureToScreenPosition } from "./gesture";
 
 export const select = (_: Gesture, latestGesture: Gesture): void => {
+  if (!latestGesture.singleGestureLandmarks || latestGesture.singleGestureLandmarks.length < 9) {
+    console.warn("[select] Not enough landmarks to select (need at least 9)", latestGesture.singleGestureLandmarks);
+    return;
+  }
   const pointerLandmark = latestGesture.singleGestureLandmarks[8];
   const screenPosition = gestureToScreenPosition(
     pointerLandmark.x,

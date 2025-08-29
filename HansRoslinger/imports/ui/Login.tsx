17cb80ab12099c7690b667579fe31456c684e391
mcra0009 <mcra0009@student.monash.edu>
2025-08-29 15:42:12 +1000

    add setAuthCookie

--- HansRoslinger/imports/ui/Login.tsx
+++ HansRoslinger/imports/ui/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser, registerUser } from "../handlers/auth/authHandlers";
+import { setAuthCookie } from "../cookies/cookies";
 import {
   Box,
   Button,

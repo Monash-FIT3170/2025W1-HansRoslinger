56ca99538c71bc6e95b8d630a4542c910fcc5506
Connor Macdougall <connoramacdougall@gmail.com>
2025-09-25 12:23:16 +1000

    Add click gesture now works like a mouse click everywhere on screen

--- HansRoslinger/imports/gesture/Click.ts
+++ HansRoslinger/imports/gesture/Click.ts
@@ -12,36 +12,32 @@ export const click = (_: Gesture, latestGesture: Gesture): void => {
     return;
   }
   const pointerLandmark = latestGesture.singleGestureLandmarks[8];
+  console.log(latestGesture.singleGestureLandmarks)
   const screenPosition = gestureToScreenPosition(
     pointerLandmark.x,
     pointerLandmark.y,
     pointerLandmark.z,
   );
 
-  // ADD IDs of other clickable (pinchable) elements here
-  const clickableElementIDs = ["gesture-detection-toggle"];
+  const target = document.elementFromPoint(screenPosition.screenX, screenPosition.screenY);
+  const rect = target?.getBoundingClientRect();
 
-  for (const clickableElementID of clickableElementIDs) {
-    const clickableElement = document.getElementById(clickableElementID);
-    const rect = clickableElement?.getBoundingClientRect();
+  if (
+    target &&
+    rect &&
+    screenPosition.screenX >= rect.left &&
+    screenPosition.screenX <= rect.right &&
+    screenPosition.screenY >= rect.top &&
+    screenPosition.screenY <= rect.bottom
+  ) {
+    const clickEvent = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: screenPosition.screenX,
+      clientY: screenPosition.screenY,
+    });
 
-    if (
-      clickableElement &&
-      rect &&
-      screenPosition.screenX >= rect.left &&
-      screenPosition.screenX <= rect.right &&
-      screenPosition.screenY >= rect.top &&
-      screenPosition.screenY <= rect.bottom
-    ) {
-      const clickEvent = new MouseEvent("click", {
-        bubbles: true,
-        cancelable: true,
-        view: window,
-        clientX: screenPosition.screenX,
-        clientY: screenPosition.screenY,
-      });
-
-      clickableElement.dispatchEvent(clickEvent);
-    }
+    target.dispatchEvent(clickEvent);
   }
 };

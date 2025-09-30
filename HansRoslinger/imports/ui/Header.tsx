d7c59fffb934cbe7ff501a0471f187c59bd7d661
Tsag0005 <111573704+Tsag0005@users.noreply.github.com>
2025-09-30 18:38:35 +1000

    Added tooltips to buttons on presenting screen

--- HansRoslinger/imports/ui/Header.tsx
+++ HansRoslinger/imports/ui/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
@@ -27,104 +27,117 @@ export const Header: React.FC<HeaderProps> = ({
   return (
     <>
       {/* Back to All Presentations */}
-      <Button
-        variant="contained"
-        onClick={() => navigate("/allpresentations")}
-        sx={{
-          width: 40,
-          height: 40,
-          minWidth: 0,
-          fontSize: "0.75rem",
-          fontWeight: "medium",
-          backgroundColor: "grey.700",
-          color: "white",
-          "&:hover": { backgroundColor: "grey.600" },
-        }}
-      >
-        Back
-      </Button>
+      <Tooltip title="Go back to all presentations" arrow>
+        <Button
+          variant="contained"
+          onClick={() => navigate("/allpresentations")}
+          sx={{
+            width: 40,
+            height: 40,
+            minWidth: 0,
+            fontSize: "0.75rem",
+            fontWeight: "medium",
+            backgroundColor: "grey.700",
+            color: "white",
+            "&:hover": { backgroundColor: "grey.600" },
+          }}
+        >
+          Back
+        </Button>
+      </Tooltip>
 
       {/* Background Removal */}
-      <Button
-        variant="contained"
-        onClick={onToggleBackgroundRemoval}
-        id="background-removal-enable"
-        sx={{
-          width: 40,
-          height: 40,
-          minWidth: 0,
-          fontSize: "0.75rem",
-          fontWeight: "medium",
-          backgroundColor: backgroundRemoval ? "cyan.400" : "grey.700",
-          color: backgroundRemoval ? "black" : "white",
-          "&:hover": {
-            backgroundColor: backgroundRemoval ? "cyan.300" : "grey.600",
-          },
-        }}
-      >
-        BR
-      </Button>
+      <Tooltip title="Toggle background removal" arrow>
+        <Button
+          variant="contained"
+          onClick={onToggleBackgroundRemoval}
+          id="background-removal-enable"
+          sx={{
+            width: 40,
+            height: 40,
+            minWidth: 0,
+            fontSize: "0.75rem",
+            fontWeight: "medium",
+            backgroundColor: backgroundRemoval ? "cyan.400" : "grey.700",
+            color: backgroundRemoval ? "black" : "white",
+            "&:hover": {
+              backgroundColor: backgroundRemoval ? "cyan.300" : "grey.600",
+            },
+          }}
+        >
+          BR
+        </Button>
+      </Tooltip>
 
       {/* Grayscale */}
-      <Button
-        variant="contained"
-        onClick={onToggleGrayscale}
-        sx={{
-          width: 40,
-          height: 40,
-          minWidth: 0,
-          fontSize: "0.75rem",
-          fontWeight: "medium",
-          backgroundColor: grayscale ? "cyan.400" : "grey.700",
-          color: grayscale ? "black" : "white",
-          "&:hover": {
-            backgroundColor: grayscale ? "cyan.300" : "grey.600",
-          },
-        }}
-      >
-        GS
-      </Button>
+      <Tooltip title="Toggle grayscale mode" arrow>
+        <Button
+          variant="contained"
+          onClick={onToggleGrayscale}
+          sx={{
+            width: 40,
+            height: 40,
+            minWidth: 0,
+            fontSize: "0.75rem",
+            fontWeight: "medium",
+            backgroundColor: grayscale ? "cyan.400" : "grey.700",
+            color: grayscale ? "black" : "white",
+            "&:hover": {
+              backgroundColor: grayscale ? "cyan.300" : "grey.600",
+            },
+          }}
+        >
+          GS
+        </Button>
+      </Tooltip>
 
       {/* Show Assets (SA) */}
-      <Button
-        variant="contained"
-        size="small"
-        sx={{
-          width: 40,
-          height: 40,
-          minWidth: 0,
-          fontSize: "0.75rem",
-          fontWeight: "medium",
-          backgroundColor: showAssets ? "cyan.400" : "grey.700",
-          color: showAssets ? "black" : "white",
-          "&:hover": {
-            backgroundColor: showAssets ? "cyan.300" : "grey.600",
-          },
-        }}
-        onClick={() => setShowAssets((s) => !s)}
-      >
-        SA
-      </Button>
+      <Tooltip title="Toggle assets view" arrow>
+        <Button
+          variant="contained"
+          size="small"
+          sx={{
+            width: 40,
+            height: 40,
+            minWidth: 0,
+            fontSize: "0.75rem",
+            fontWeight: "medium",
+            backgroundColor: showAssets ? "cyan.400" : "grey.700",
+            color: showAssets ? "black" : "white",
+            "&:hover": {
+              backgroundColor: showAssets ? "cyan.300" : "grey.600",
+            },
+          }}
+          onClick={() => setShowAssets((s) => !s)}
+        >
+          SA
+        </Button>
+      </Tooltip>
 
       {/* Chart Toggle */}
-      <Button
-        variant="contained"
-        onClick={onToggleChart}
-        sx={{
-          width: 40,
-          height: 40,
-          minWidth: 0,
-          fontSize: "0.75rem",
-          fontWeight: "medium",
-          backgroundColor: "cyan.400",
-          color: "black",
-          "&:hover": {
-            backgroundColor: "cyan.300",
-          },
-        }}
+      <Tooltip
+        title={showLineChart ? "Switch to bar chart" : "Switch to line chart"}
+        arrow
       >
-        {showLineChart ? "Bar" : "Line"}
-      </Button>
+        <Button
+          variant="contained"
+          onClick={onToggleChart}
+          sx={{
+            width: 40,
+            height: 40,
+            minWidth: 0,
+            fontSize: "0.75rem",
+            fontWeight: "medium",
+            backgroundColor: "cyan.400",
+            color: "black",
+            "&:hover": {
+              backgroundColor: "cyan.300",
+            },
+          }}
+        >
+          {showLineChart ? "Bar" : "Line"}
+        </Button>
+      </Tooltip>
     </>
   );
 };

5df5c06602d708fb18a88fdfc18e701ef6b283ed
Nathan Michailidis <nmic0005@student.monash.edu>
2025-10-10 07:37:37 +1100

    state history saving/loading

--- HansRoslinger/imports/api/stateHistory.tsx
+++ HansRoslinger/imports/api/stateHistory.tsx
@@ -1,31 +1,41 @@
 
 
 const stateHistory = [null, null, null, null, null];
-var stateRetrieved = true
-var startTime = 0
+var lastLoadTime = 0
+var loadIndex = 0
+var saveIndex = 0
 
-function getState() {
-    const state = stateHistory[0]
+function loadState() {
+    if(time.current() - lastLoadTime > 1000) {
+        loadIndex = 0
+    }
 
-    stateHistory[0] = stateHistory[1]
-    stateHistory[1] = stateHistory[2]
-    stateHistory[2] = stateHistory[3]
-    stateHistory[3] = stateHistory[4]
-    stateHistory[4] = null
-
-    stateRetrieved = true
-    startTime = time.current()
+    var state;
+    if (loadIndex < saveIndex) {
+        state = stateHistory[loadIndex]
+        loadIndex = loadIndex + 1
+        lastLoadTime = time.current()
+    } else {
+        state = null
+    }
+   
     return state
 }
 
-function saveState() {
-    if(stateRetrieved && time.current() - startTime > 1000) {
-        stateHistory[0] = null
-        stateHistory[1] = null
-        stateHistory[2] = null
-        stateHistory[3] = null
-        stateHistory[4] = null
-        stateRetrieved = false
-    } else if (time.current() - startTime > 1000) {
-        // Save
+function saveState(state: any) {
+    if(time.current() - lastLoadTime > 1000) {
+        // Reset
+        saveIndex = 0
+    } 
+
+    stateHistory[4] = stateHistory[3]
+    stateHistory[3] = stateHistory[2]
+    stateHistory[2] = stateHistory[1]
+    stateHistory[1] = stateHistory[0]
+    stateHistory[0] = state
+
+    if (saveIndex < 5) {
+        saveIndex = saveIndex + 1
     }
+}
+    
